e23470c71f438cd074bad371ef7c42d69ef85e3f
taylorwentzel <66229191+taylorwentzel@users.noreply.github.com>
2020-06-12 15:56:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
-
-    my_output = f"Hello {my_input}"
-
-    print(f"::set-output name=myOutput::{my_output}")
+    payload = {'name':'Kit Plummer', 'username': 'kitplummer'}
+    r = requests.post('https://ghatracker.herokuapp.com/', json=payload)
 
 
 if __name__ == "__main__":
